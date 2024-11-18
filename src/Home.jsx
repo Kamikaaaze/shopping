985022e026e9135e84e6e86b7ae7c06e2e0b922f
aswin us <20br13512@rit.ac.in>
2024-11-18 21:52:04 +0530

    text written

---
+++ src/Home.jsx
@@ -0,0 +1,22 @@
+import './Home.css'
+function Home(){
+return(
+<div className='parent'>
+<div className='content'>
+    <p className='title'>BASE<br></br> APPAREL</p>
+    <p style={{fontFamily:'arial',fontSize:'60px',lineHeight:'0px'}}>WE'RE </p>
+    <p style={{fontFamily:'arial',fontSize:'60px',lineHeight:'0px'}}>COMING</p>
+    <p style={{fontFamily:'arial',fontSize:'60px',lineHeight:'0px'}}>SOON</p>
+
+    <p className='ad'>Hello fellow shoppers happy to announce our new wedding fashion launched in our grand inaugration happeing in kochi.</p>
+</div>
+<div><img src='./img/winona.jpg' className='thumb'/></div>
+</div>
+
+
+
+    
+)
+
+}
+export default Home;
