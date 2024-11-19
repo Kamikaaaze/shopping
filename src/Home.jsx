6f2f592f068de8cec4d19e86023033de876fcbfb
aswin us <20br13512@rit.ac.in>
2024-11-19 15:11:33 +0530

    added email

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,14 +3,20 @@ function Home(){
 return(
 <div className='parent'>
 <div className='content'>
+    <div className='circle'></div>
     <p className='title'>BASE<br></br> APPAREL</p>
-    <p style={{fontFamily:'arial',fontSize:'60px',lineHeight:'0px'}}>WE'RE </p>
-    <p style={{fontFamily:'arial',fontSize:'60px',lineHeight:'0px'}}>COMING</p>
-    <p style={{fontFamily:'arial',fontSize:'60px',lineHeight:'0px'}}>SOON</p>
+    <div className='extra'>
+    <p  style={{fontFamily:'sans-serif',fontSize:'60px',lineHeight:'0px', color:'rgb(193, 151, 153)',fontWeight:300 }}>WE'RE </p>
+    <p style={{fontFamily:'arial',fontSize:'60px',lineHeight:'0px', fontWeight:600}}>COMING</p>
+    <p style={{fontFamily:'arial',fontSize:'60px',lineHeight:'0px', fontWeight:600,marginBottom:'5px'}}>SOON</p>
+    </div>
+    
 
     <p className='ad'>Hello fellow shoppers happy to announce our new wedding fashion launched in our grand inaugration happeing in kochi.</p>
+    <input className='email' placeholder='Email Address' type='text'/>
+    <input type='submit' placeholder="hi"/>
 </div>
-<div><img src='./img/winona.jpg' className='thumb'/></div>
+<div><img src='./img/winona.jpg' className='thumb' /></div>
 </div>
 
 
